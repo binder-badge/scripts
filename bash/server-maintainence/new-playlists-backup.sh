5aab0d19b35a67a095a7235063d811b171feedb6
binder-badge <124085323+binder-badge@users.noreply.github.com>
2025-10-16 17:39:54 -0400

    remove navidrome info

---
+++ bash/server-maintainence/new-playlists-backup.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+# user variables
+url="music.local.cheaping.top"
+user="Guest"
+password="guest"
+salt="123456"
+playlistFolder="/media/1TBStorage/samba/Music/Playlists"
+
+subsonic_ver="1.16.1"
+
+hash=$(echo -n $password$salt | md5sum | cut -d " " -f1)
+
+find "$playlistFolder" -name '*.m3u' -type f -delete
+
+curl "https://$url/rest/getPlaylists.view?u=$user&t=$hash&s=$salt&v=$subsonic_ver&c=curl&f=json" -o output.json
+
+playlistCount=$(jq '."subsonic-response".playlists.playlist | length' output.json)
+
+for ((i = 0 ; i < $playlistCount ; i++ ));
+do
+    playlistName=$(jq -r '."subsonic-response".playlists.playlist['$i'].name' output.json)
+    playlistFilePath=$playlistFolder/$playlistName.m3u
+    
+    # regex line 1: /music/test/test.flac >/../test/test.flac
+    # regex line 2: removes lines that start with #
+    # regex line 3: removes new lines
+        
+    docker exec navidrome /app/navidrome pls -n -p $playlistName |& 
+        sed 's/^\/music\//\.\.\//g;
+            /^#.*$/d; 
+            /^$/d' > "$playlistFilePath"
+
+done
